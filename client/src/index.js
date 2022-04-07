df3b874769ff62b115208ee9453ee0a475c336cf
sohanr <mizan.rahman66d@gmail.com>
2022-04-07 20:32:18 +0600

    moved to reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOMClient from "react-dom/client";
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from "redux";
+import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import App from './App';
 import './index.css';
+import rootReducer from './reducers';
 import reportWebVitals from './reportWebVitals';
 
 
-// create user reducer function
-const authReducer = (state = {}, action) =>{
-  switch (action.type) {
-    case "LOGGED_IN_USER":
-      return {...state, ...action.payload}
-    
-    case "LOGOUT":
-      return action.payload;    
 
-    default:
-      return state;
-  }
-}
 
-// combine multiple reducer
-
-const rootReducer = combineReducers({
-  user: authReducer,
-})
 
 // create redux store
 const store = createStore(rootReducer, composeWithDevTools());
