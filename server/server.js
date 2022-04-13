1721fd5295da87bd30363f56fccfe0f297cc8134
sohanr <mizan.rahman66d@gmail.com>
2022-04-13 17:48:01 +0600

    middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
+import cors from 'cors';
 import express from "express";
 import fs from 'fs';
+require("dotenv").config();
+const morgan = require("morgan");
+
+
+
 
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
 
 // route middleware
 fs.readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
@@ -10,4 +20,6 @@ fs.readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 
 
 
-app.listen(3010, () => console.log("book my show server is running"))
+const port = process.env.PORT || 3010;
+
+app.listen(port, () => console.log(`book my show server is running on port ${port} `))
