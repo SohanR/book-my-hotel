1db5908eca82d50c05692ab9e08507c2db154440
sohanr <mizan.rahman66d@gmail.com>
2022-04-13 16:20:09 +0600

    middleware setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import fs from 'fs';
 
 
 const app = express();
 
-app.get("/")
+// route middleware
+fs.readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
+
+
+
 
 app.listen(3010, () => console.log("book my show server is running"))
