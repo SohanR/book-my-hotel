77262568c54feed8827b4fbc15408e9c4d9c89f8
sohanr <mizan.rahman66d@gmail.com>
2022-04-19 04:53:34 +0600

    register

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,2 +1,6 @@
 export const showMessage = (req, res) => {
     res.status(200).send(`Here is your message: ${req.params.message}`)}
+
+export const register = async (req, res ) =>{
+    console.log(req.body);
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { showMessage } from "../controller/auth";
+import { register, showMessage } from "../controller/auth";
 
 const router = express.Router();
 
 
 router.get("/:message", showMessage);
+router.post("/register", register)
 
 module.exports = router;
