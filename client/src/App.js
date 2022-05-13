d03157b4241066ed4cecd5d9e49b3699a866c6ee
sohanr <mizan.rahman66d@gmail.com>
2022-05-14 02:49:39 +0600

    top nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Home from "./booking/Home";
+import TopNav from "./components/TopNav";
+
 
 function App() {
   return (
     <BrowserRouter>
+    <TopNav/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Login" component={Login} />
