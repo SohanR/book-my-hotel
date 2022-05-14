8cdb4d1d3a35c90a0f6674d53e675287d16b3809
sohanr <mizan.rahman66d@gmail.com>
2022-05-15 02:40:49 +0600

    controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { showMessage } from "../controllers/auth";
 
 const router = express.Router();
 
-router.get('/:message', (req, res) =>{
-    res.status(200).send(`${req.params.message}`)
-})
+router.get('/:message', showMessage)
+
 
 
 
