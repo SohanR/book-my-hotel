1ef6e79b8209c33a4932c39bfabd95148ef74eab
sohanr <mizan.rahman66d@gmail.com>
2022-05-16 19:51:31 +0600

    register endpoint

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import RegisterForm from '../components/RegisterForm';
 
@@ -7,9 +8,19 @@ const Register = () => {
   const [password, setPassword] = useState("");
 
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault();
     console.table({name, email, password});
+
+    try {
+      const res = await axios.post(`http://localhost:8000/api/register`, {
+      name, email, password
+    })
+
+    console.log(" Register User ===>>>" , res);
+    } catch (error) {
+      console.log("Registration error: ", error);
+    }
   }
 
   return (
