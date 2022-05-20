62c2ee54e69666322e2103ce28784a50a63a8dc3
sohanr <mizan.rahman66d@gmail.com>
2022-05-20 22:17:05 +0600

    log in route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { register } from "../controllers/auth";
+import { login, register } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post('/register', register)
+
+router.post('/login', login)
 
 
 
