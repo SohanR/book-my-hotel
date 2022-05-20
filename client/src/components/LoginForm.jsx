ff810203fdbd061e9f4699246bdc9d9ebb5042d4
sohanr <mizan.rahman66d@gmail.com>
2022-05-20 19:04:19 +0600

    login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const LoginForm = () => {
+const LoginForm = ({ handleSubmit, email, setEmail, password, setPassword }) => {
   return (
-    <div>LoginForm</div>
+    <form onSubmit={handleSubmit} className="mt-3">
+
+        <div className='form-group mb-3' >
+            <label className="form-label" >Your Email</label>
+            <input 
+                type="email"
+                className="form-control"
+                placeholder="Enter your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}            
+            />
+        </div>
+
+        <div className='form-group mb-3' >
+            <label className="form-label" >Password</label>
+            <input 
+                type="password"
+                className="form-control"
+                placeholder="Enter your Name"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}            
+            />
+        </div>
+
+        <button disabled={!email || !password} className="btn btn-primary" >Submit</button>
+    </form>
   )
 }
 
-export default LoginForm
+export default LoginForm;
