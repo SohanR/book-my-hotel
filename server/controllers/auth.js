530306e1ada5229146439522a3674e968d33b11a
sohanr <mizan.rahman66d@gmail.com>
2022-05-20 22:24:44 +0600

    check user and compare login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/user";
 
+
+// for register
 export const register = async (req, res) => {
     console.log(req.body);
 
@@ -33,3 +35,39 @@ export const register = async (req, res) => {
 
 
 }
+
+// for login 
+
+export const login = async(req, res) =>{
+    console.log(req.body);
+
+    const {email, password} = req.body;
+
+
+    try {
+        // check if user with that email exist
+        let user = await User.findOne({email}).exec();
+        console.log('USER EXIST: ', user);
+
+        // if no user
+        if(!user){
+         res.status(400).send("User not found, Please register for log in")        
+        }
+
+        // compare password
+        user.comparePassword(password, (err, match) =>{
+            console.log('COMPARE PASSWORD IN LOG IN ERR: ',err);
+
+            if(!match || err) return res.status(400).send("Incorrect password")
+
+            console.log('GANERATED A TOKEN THEN SEND AS RESPONSE TO CLIENT');
+        })
+            
+        
+        
+    } catch (error) {
+        console.log("LOG IN ERROR", error);
+
+        res.status(400).send("Singin failed")
+    }
+} 
