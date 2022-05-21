97cfe3c062e0074d597792ee19b86f0ee93872e9
sohanr <mizan.rahman66d@gmail.com>
2022-05-21 18:51:18 +0600

    logout process

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 const TopNav = () => {
+
+  const { auth } = useSelector((state) => ({...state}))
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // log out functionality
+  const logout = () =>{
+    dispatch({
+      type: 'LOGOUT',
+      payload:null
+    })
+    window.localStorage.removeItem("auth")
+    toast.info("You are logged out")
+    navigate('/login')
+  }
+
+
   return (
     <div className='nav bg-light d-flex justify-content-between' >
         <Link className="nav-link" to="/">Home</Link>
 
-        <Link className="nav-link" to="/login">Login</Link>
 
-        <Link className="nav-link" to="/register">Register</Link>
+      {auth !== null && (<button  onClick={logout} className="btn btn-outline-danger m-r-3">Logout</button>)}
+
+
+
+
+
+
+
+        {auth === null && (
+          <>
+            <Link className="nav-link" to="/login">Login</Link>
+
+            <Link className="nav-link" to="/register">Register</Link>
+          </>
+        )}
+
+
     </div>
   )
 }
