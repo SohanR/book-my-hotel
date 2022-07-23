2e6a4a23c6dc7596ece326e384149526eb250f25
sohanr <mizan.rahman66d@gmail.com>
2022-07-24 02:21:31 +0600

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Home from "./booking/Home";
+import PrivateRoute from "./components/PrivateRoute";
 import TopNav from "./components/TopNav";
+import Dashboard from "./user/Dashboard";
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/login" element={<Login/>} />
         <Route exact path="/register" element={<Register/>} />
+        <PrivateRoute exact path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </BrowserRouter>
     
