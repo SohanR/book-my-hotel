1b74709c2cf121587aba054a420424166e6926f7
sohanr <mizan.rahman66d@gmail.com>
2022-07-26 01:14:11 +0600

    homepage

--- client/src/booking/Home.jsx
+++ client/src/booking/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+
 
 const Home = () => {
-
-  const state = useSelector((state) => state )
   return (
-    <div className='container-fluid h1 p-5 text-center' >{JSON.stringify(state)}</div>
+    <div className='container-fluid h1 p-5 text-center' >Home</div>
   )
 }
 
