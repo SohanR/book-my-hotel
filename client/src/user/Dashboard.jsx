b06605e5d35456fa13868c5ea39d524c0a619092
sohanr <mizan.rahman66d@gmail.com>
2022-07-29 02:22:57 +0600

    nav

--- client/src/user/Dashboard.jsx
+++ client/src/user/Dashboard.jsx
@@ -1,4 +1,13 @@
+// show a navbar in dashboard..... it will show 2 components
+// 1 for booking and a button to browse hotels
+// 2 for show all hotels you have posted and a button to add new
+// use tabs as nav
+
+
+
+
 import React from 'react'
+import DashboardNav from '../components/DashboardNav'
 
 const Dashboard = () => {
   return (
@@ -6,7 +15,7 @@ const Dashboard = () => {
         <div className="container-fluid bg-secondary p-5">
             <h1>Dashboard</h1>
         </div>
-
+            <DashboardNav/>
         <div className="container">
             <p>Show all bookings and a button to browse hotels</p>
         </div>
