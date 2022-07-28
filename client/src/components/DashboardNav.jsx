6ec6ee8ca89d23215aa985c4c93bd504e2b53be1
sohanr <mizan.rahman66d@gmail.com>
2022-07-29 02:21:29 +0600

    active link

--- client/src/components/DashboardNav.jsx
+++ client/src/components/DashboardNav.jsx
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import React from 'react'
+import React from 'react';
 
 const DashboardNav = () => {
+
+  const active = window.location.pathname;
+
   return (
     <ul className="nav nav-tabs">
         <li className="nav-item">
-            <Link to='/dashboard'>Your Bookings</Link>
+            <Link className={`nav-link ${active === "/dashboard" && "active"}`} to='/dashboard'>Your Bookings</Link>
         </li>
 
         <li className="nav-item">
-            <Link to='/dashboard/seller'>Your Hotels</Link>
+            <Link className={`nav-link ${active === "/dashboard/seller" && "active primary"}`} to='/dashboard/seller'>Your Hotels </Link>
         </li>
 
     </ul>
