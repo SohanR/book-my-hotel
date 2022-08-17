dd089b7625e5f53fd98356daf90beb5ebb41a02e
sohanr <mizan.rahman66d@gmail.com>
2022-08-17 18:08:06 +0600

    req auth

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,3 +1,4 @@
 export const createConnectAccount = async (req, res) =>{
+    console.log("REQ USER FROM REQUIRE_SINGIN MIDDLEWARE",  req.auth); //  we can get headers by req.headers
     console.log("you hit create connect account endpoind");
 } 
