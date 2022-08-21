0e41ffb2cdeef5cc7765aa8a76e126dfb4750d07
sohanr <mizan.rahman66d@gmail.com>
2022-08-21 20:36:25 +0600

    update local storage

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,3 +7,15 @@ export const register = async (user ) => await axios.post(`${process.env.REACT_A
 
 // for log in
 export const login = async (user ) => await axios.post(`${process.env.REACT_APP_API}/login`, user); 
+
+
+// update user in local storage
+export const updateUserInLocalStorage = (user, next) =>{
+    if(window.localStorage.getItem('auth')){
+        let auth = JSON.parse(localStorage.getItem('auth'));
+
+        auth.user = user;
+        localStorage.setItem("auth", JSON.stringify(auth));
+        next();
+    }
+}
