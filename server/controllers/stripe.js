18cc0290e20bf6a0f292cb1eba9daeff44585dc9
sohanr <mizan.rahman66d@gmail.com>
2022-09-09 23:06:28 +0600

    fixed auth error

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -93,8 +93,9 @@ export const getAccountStatus = async (req,res) => {
 
 //getting the account balance
 export const getAccountBalance = async (req, res) =>{
-    const user = await User.findById(req.user._id).exec();
-
+    const user = await User.findById(req.auth._id).exec();
+    
+    
     try {
         const balance = await stripe.balance.retrieve({
             stripeAccount: user.stripe_account_id
