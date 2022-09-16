9588624da475dfde89ae17e995f5f8be130c01d3
sohanr <mizan.rahman66d@gmail.com>
2022-09-16 20:32:45 +0600

    input field

--- client/src/hotels/NewHotel.jsx
+++ client/src/hotels/NewHotel.jsx
@@ -39,7 +39,17 @@ const NewHotel = () => {
           Image
           <input type="file" name='image' onChange={handleImageChange} accept='image/*'  hidden/>
         </label>
+
+        <input type="text" name="title" onChange={handleChange} placeholder='Title' className='form-control m-2' value={title} />
+
+        <textarea type="text" name="content" onChange={handleChange} placeholder='Content' className='form-control m-2' value={content} />
+
+        <input type="number" name="price" onChange={handleChange} placeholder='Price' className='form-control m-2' value={price} />
+
+        <input type="number" name="bed" onChange={handleChange} placeholder='Number of Beds' className='form-control m-2' value={bed} />
       </div>
+
+      <button className='btn btn-outline-primary m-2' >Save</button>
     </from>
   )
 
@@ -58,7 +68,7 @@ const NewHotel = () => {
             </div>
 
             <div className='col-md-2' >
-              image
+              image <pre>{JSON.stringify(values, null, 4)}</pre>
             </div>
           </div>
         </div> 
