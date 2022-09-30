0862813253e488d898d4a7e9c9639e2d34267ca4
sohanr <mizan.rahman66d@gmail.com>
2022-10-01 02:23:24 +0600

    hotel action

---
+++ client/src/actions/hotel.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const createHotel = async (token, data) => await axios.post(`${process.env.REACT_APP_API}/create-hotel`, data, {
+    headers:{
+        Authorization: `Bearer ${token}`
+    }
+})
