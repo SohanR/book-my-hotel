53988426ca08568aec3893fa29b9d81971f3f5d7
sohanr <mizan.rahman66d@gmail.com>
2022-10-01 02:23:49 +0600

    component based

--- client/src/hotels/NewHotel.jsx
+++ client/src/hotels/NewHotel.jsx
@@ -1,104 +1,34 @@
-import AlgoliaPlaces from 'algolia-places-react';
-import { DatePicker, Select } from 'antd';
-import moment from 'moment';
 import React, { useState } from 'react';
+import HotelForm from '../components/HotelForm';
 
-const config = {
-  appId:process.env.REACT_APP_ALGOLIA_APP_ID,
-  apiKey:process.env.REACT_APP_ALGOLIA_API_KEY,
-}
+// const config = {
+//   appId:process.env.REACT_APP_ALGOLIA_APP_ID,
+//   apiKey:process.env.REACT_APP_ALGOLIA_API_KEY,
+// }
 
-const {Option} = Select;
+
 
 
 const NewHotel = () => {
-
+  
 //state
-  const [values, setValues] = useState({
-    title: '',
-    content: '',
-    location: '',
-    image: '',
-    price: '',
-    from: '',
-    to: '',
-    bed:''
-  });
-
-  const [preview, setpreview] = useState("https://via.placeholder.com/150/?text=PREVIEW");
-
-  //destructuring values
-  const {title,content,location,image,price,from,to,bed} = values;
+const [values, setValues] = useState({
+  title: '',
+  content: '',
+  location: '',
+  image: '',
+  price: '',
+  from: '',
+  to: '',
+  bed:''
+});
 
 
-  //event handler
-  const handleSubmit = () =>{
-
-  }
-
-  const handleImageChange = (e) =>{
-    //console.log( e.target.files[0]);
-    setpreview(URL.createObjectURL(e.target.files[0]));
-    setValues({...values, image:e.target.files[0]})
-
-  }
-
-  const handleChange = (e) =>{
-    setValues({...values, [e.target.name] : e.target.value })
-  }
 
 
-  const hotelForm = () =>(
-    <form onSubmit={handleSubmit} >
-      <div className='form-group' >
-        <label  className='btn btn-outline-secondary btn-block m-2 text-left'>
-          Image
-          <input type="file" name='image' onChange={handleImageChange} accept='image/*'  hidden/>
-        </label>
+  const [preview, setPreview] = useState("https://via.placeholder.com/150/?text=PREVIEW");
 
-        <input type="text" name="title" onChange={handleChange} placeholder='Title' className='form-control m-2' value={title} />
 
-        <textarea type="text" name="content" onChange={handleChange} placeholder='Content' className='form-control m-2' value={content} />
-
-        <AlgoliaPlaces className='form-control ml-2 mr-2' placeholder='location' defaultValue={location} optiona={config} onChange={({suggestion}) => setValues({...values, location: suggestion.value}) } style={{height:"50px"}}
-        />
-
-        <input type="number" name="price" onChange={handleChange} placeholder='Price' className='form-control m-2' value={price} />
-
-        {/* <input type="number" name="bed" onChange={handleChange} placeholder='Number of Beds' className='form-control m-2' value={bed} /> */}
-
-        <Select onChange={(value) => setValues({...values, bed:value })} className='w-100 m-2' placeholder='Number of Beds' >
-          <Option key={1} >1</Option>
-          <Option key={2} >2</Option>
-          <Option key={3} >3</Option>
-          <Option key={4} >4</Option>
-
-          
-        </Select>
-
-      </div>
-
-      <DatePicker 
-        placeholder='From Date' 
-        className='form-control m-2' 
-        onChange={( date, dateString ) => 
-          setValues({...values, from:dateString})}  
-        disabledDate={(current) => 
-          current && current.valueOf() < moment().subtract(1, 'days')}
-        />
-
-      <DatePicker 
-        placeholder='To Date' 
-        className='form-control m-2' 
-        onChange={( date, dateString ) => 
-          setValues({...values, to:dateString})} 
-        disabledDate={(current) => 
-          current && current.valueOf() <   moment().subtract(1, 'days')}
-      />
-
-      <button className='btn btn-outline-primary m-2' >Save</button>
-    </form>
-  )
 
   return (
     <>
@@ -111,7 +41,7 @@ const NewHotel = () => {
             <div className='col-md-10' >
               <br />
               
-              {hotelForm()}
+             <HotelForm values={values} setValues={setValues} setpreview={setPreview} />
             </div>
 
             <div className='col-md-2' >
