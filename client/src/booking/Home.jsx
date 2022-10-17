44aab0a375561474134c5f55a2f44c969f53bc48
sohanr <mizan.rahman66d@gmail.com>
2022-10-17 23:33:48 +0600

    hotel data in homepage

--- client/src/booking/Home.jsx
+++ client/src/booking/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { allHotels } from '../actions/hotel';
+import SmallCard from '../components/cards/SmallCard';
 
 const Home = () => {
+  
+  const [hotel, setHotel] = useState([]);
+
+  useEffect(() => {
+    loadAllHotels()
+  }, []);
+
+
+
+  const loadAllHotels =async () =>{
+    let res = await allHotels();
+
+    setHotel(res.data)
+  }
+
   return (
-    <div className='container-fluid h1 p-5 text-center' >Home</div>
+    <>
+      <div className='container-fluid p-5 text-center' >
+        <h1>All Hotels</h1>
+      </div>
+
+      <div className='container-fluid'>
+        {/* <pre>
+          {JSON.stringify(hotel, null, 1)}
+        </pre> */}
+
+        {hotel.map((h) => <SmallCard key={h._id} h={h} />)}
+      </div>    
+    </>
+
+
   )
 }
 
