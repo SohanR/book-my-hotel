fed73f7f11a256d66c5e354083e8523e250ac94e
sohanr <mizan.rahman66d@gmail.com>
2022-11-06 13:59:23 +0600

    debug headers errors

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,27 +4,30 @@ import User from "../models/user";
 
 // for register
 export const register = async (req, res) => {
-    console.log(req.body);
 
-
-    // saving user data in database
-    const {name, email, password} = req.body;
-
-    //validation
-    if(!name) return res.status(400).send("Name is required")
-    if(!password) return res.status(400).send("Password is required")
-    if(password.length < 6) return res.status(400).send("Password should be minimum 6 characters long")
-
-    // cant save 2 user with same email
-    let userExist = await User.findOne({email}).exec()
-
-    if(userExist) return res.status(400).send("Email is Taken")
-
-    // create registered user
-    const user = new User(req.body)
-
-    // now save data 
     try {
+
+        console.log(req.body);
+
+
+        // saving user data in database
+        const {name, email, password} = req.body;
+    
+        //validation
+        if(!name) return res.status(400).send("Name is required")
+        if(!password) return res.status(400).send("Password is required")
+        if(password.length < 6) return res.status(400).send("Password should be minimum 6 characters long")
+    
+        // cant save 2 user with same email
+        let userExist = await User.findOne({email}).exec()
+    
+        if(userExist) return res.status(400).send("Email is Taken")
+    
+        // create registered user
+        const user = new User(req.body)
+    
+        // now save data 
+
         await user.save()
         console.log("User Created: ", user);
         return res.json({ok:true})
