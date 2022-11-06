81aae59ee918bc4c5473aa4f7ce3651b64ef1481
sohanr <mizan.rahman66d@gmail.com>
2022-11-06 14:08:49 +0600

    get hoetl postedby data

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -69,3 +69,14 @@ export const image = async (req, res) =>{
         return res.send(hotel.image.data);
     }
 }
+
+
+// for getting seller hotels data
+export const sellerHotels = async (req, res) =>{
+    let all = await Hotel.find({postedBy:req.user._id})
+    .select('-image.data')
+    .populate('postedBy', '_id name')
+    .exec();
+
+    res.send(all);
+}
