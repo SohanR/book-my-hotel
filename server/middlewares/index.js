c316313b13aa31d7f6c310069a03dfc70941f2d2
sohanr <mizan.rahman66d@gmail.com>
2022-11-19 00:29:59 +0600

    check owner

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,22 @@
 import { expressjwt } from "express-jwt";
+import Hotel from "./../models/hotel";
 
 export const requireSingin = expressjwt({
     secret: process.env.JWT_SECRET,
     algorithms:["HS256"]
 })
+
+
+// hotel owner check
+export const hotelOwner = async (req, res , next) =>{
+    let hotel = await Hotel.findById(req.params.hotelId).exec();
+
+    let owner = hotel.postedBy._id.toString() === req.auth._id.toString();
+
+    if(!owner){
+        return res.status(404).send("Unauthorized")
+    }
+
+
+    next()
+}
