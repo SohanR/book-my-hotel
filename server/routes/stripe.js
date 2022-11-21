b1f5615d4ea2754f5fd394fa59a481eae1e61b6c
sohanr <mizan.rahman66d@gmail.com>
2022-11-21 17:33:20 +0600

    strip session endpoint

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // middleware
 import { requireSingin } from "../middlewares";
 // controller
-import { createConnectAccount, getAccountBalance, getAccountStatus, payoutSetting } from "../controllers/stripe";
+import { createConnectAccount, getAccountBalance, getAccountStatus, payoutSetting, stripSessionId } from "../controllers/stripe";
 
 
 
@@ -20,5 +20,7 @@ router.post("/get-account-status", requireSingin, getAccountStatus)
 router.post("/get-account-balance", requireSingin, getAccountBalance )
 
 router.post("/payout-setting", requireSingin, payoutSetting)
+
+router.post("/stripe-session-id", requireSingin, stripSessionId)
 
 module.exports = router;
