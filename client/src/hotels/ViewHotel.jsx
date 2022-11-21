2c087a1bf2d3354d780b6d38c8a0db878d8b5f49
sohanr <mizan.rahman66d@gmail.com>
2022-11-21 15:12:25 +0600

    single hotel view

---
+++ client/src/hotels/ViewHotel.jsx
@@ -0,0 +1,78 @@
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { diffDays, read } from '../actions/hotel';
+
+const ViewHotel = () => {
+
+    const [hotel, setHotel] = useState({});
+    const [image, setImage] = useState("");
+
+    const {hotelId} = useParams()
+
+    useEffect(() => {
+        loadHotel()
+    }, []);
+
+
+    const loadHotel = async () =>{
+        let res = await read(hotelId)
+
+        setHotel(res.data)
+        setImage(`${process.env.REACT_APP_API}/hotel/image/${res.data._id}`)
+
+    }
+
+  return (
+    <>
+        <div className='container-fluid bg-secondary p-5 text-center' >
+            <h2>{hotel.title}</h2>
+        </div>
+
+        <div className="container-fluid" >
+            <div className="row">
+                <div className="col-md-6">
+                    <br />
+
+                    <img src={image} alt={hotel.title} className="img img-fluid m-2" />
+                </div>
+
+                <div className="col-md-6">
+                    <br />
+                    <b>{hotel.content}</b>
+                    <p className='alert alert-info mt-3' > BDT {hotel.price}.00  </p>
+
+                    <p className="card-text">
+                      <span className='float-right text-primary' >
+                         for {diffDays(hotel.from, hotel.to)}
+                         {diffDays(hotel.from, hotel.to) <= 1 ? ' Day' : ' Days' }
+                      </span>
+                    </p>
+
+                    <p>
+                        From <br /> {moment(new Date(hotel.from)).format("MMMM Do YYYY")}
+                    </p>
+
+                    <p>
+                        To <br /> {moment(new Date(hotel.to)).format("MMMM Do YYYY")}
+                    </p>
+
+                    
+
+
+                    <i className='text-success'>Posted By {hotel.postedBy && hotel.postedBy.name.toUpperCase()}</i>
+                    
+                    <br />
+
+                
+                    <button className='btn btn-block btn-lg btn-primary mt-3 ' >Book Now</button>
+                </div>
+            </div>
+        </div>
+
+        
+    </>
+  )
+}
+
+export default ViewHotel
