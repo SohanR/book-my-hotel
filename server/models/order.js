5f42310aac9b36ea80b82bb6362c18abe9e2b638
sohanr <mizan.rahman66d@gmail.com>
2022-11-22 22:10:24 +0600

    oder model

---
+++ server/models/order.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const {ObjectId} = mongoose.Schema;
+
+
+const orderSchema = new mongoose.Schema({
+    hotel:{
+        type:ObjectId,
+        ref:"Hotel"
+    },
+    session:{},
+    orderedBy:{
+        type:ObjectId,
+        ref:"User"
+    },
+}{timestamps:true})
+
+module.exports = mongoose.model("Order",orderSchema);
