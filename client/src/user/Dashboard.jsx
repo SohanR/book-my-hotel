93c6b536161c7a2da1cbe8809f239b4754374037
sohanr <mizan.rahman66d@gmail.com>
2022-11-23 15:45:18 +0600

    showing booking info

--- client/src/user/Dashboard.jsx
+++ client/src/user/Dashboard.jsx
@@ -6,12 +6,32 @@
 
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { userHotelBookings } from '../actions/hotel'
 import ConnectNav from '../components/ConnectNav'
 import DashboardNav from '../components/DashboardNav'
 
 const Dashboard = () => {
+
+  const {auth} = useSelector((state) => state )
+  const {token} = auth;
+
+  const [booking, setBooking] = useState([]);
+
+  useEffect(() => {
+    loadUserBookings()
+    
+  }, []);
+
+
+  const loadUserBookings = async () => {
+    const res = await userHotelBookings(token);
+
+    console.log(res);
+    setBooking(res.data)
+  }
   return (
     <>
         <div className="container-fluid bg-secondary p-5">
@@ -29,10 +49,14 @@ const Dashboard = () => {
             <div className='col-md-2'>
               <Link to='/' className='btn btn-primary' > Browse Hotels</Link>
             </div>
-          </div>          
+          </div>    
+
+          <div className="row">
+            <pre>{JSON.stringify(booking,null,4)}</pre>  
+          </div>      
         </div>
     </>
-  )
+  ) 
 }
 
 export default Dashboard
