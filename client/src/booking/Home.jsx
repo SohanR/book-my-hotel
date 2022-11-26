8508c1c2e3b39dc904246bdc9d61c1ea2d28a463
sohanr <mizan.rahman66d@gmail.com>
2022-11-26 13:43:28 +0600

    landing page

--- client/src/booking/Home.jsx
+++ client/src/booking/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Destination from '../components/Destination'
+import DownloadApp from '../components/DownloadApp'
+import Search from '../components/forms/Search'
+import Main from '../components/Main'
+import Offer from '../components/Offer'
+import Service from '../components/Service'
+import Testimonial from '../components/Testimonial'
+import Tours from '../components/Tours'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <Main/>
+     <Search/>
+     <Service/>
+     <Destination/>
+      <Offer />
+      <Tours />
+      <Testimonial />
+      <DownloadApp />
+    </>
   )
 }
 
