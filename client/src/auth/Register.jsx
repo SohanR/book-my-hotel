8e0c81f5541357fec70787b1a5bca1a9c1a63c7c
sohanr <mizan.rahman66d@gmail.com>
2023-01-18 00:57:03 +0600

    regex applied for name

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -5,6 +5,7 @@ import { register } from '../actions/auth';
 import RegisterForm from '../components/forms/RegisterForm';
 
 const Register = () => {
+  const validName = new RegExp(/^[a-z][a-z\s]*$/);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,21 +16,28 @@ const Register = () => {
     e.preventDefault();
     console.table({name, email, password});
 
-    try {
-      const res = await register({
-        name, email, password
-      })
-
-    console.log(" Register User ===>>>" , res);
-    toast.success("Registration Success, Please log in!")
-    navigate("/login")
-    } catch (error) {
-      console.log("Registration error: ", error);
-      if(error.response.status === 400){
-        toast.error(error.response.data)
+    if (validName.test(name)) {
+      try {
+        const res = await register({
+          name, email, password
+        })
+  
+      console.log(" Register User ===>>>" , res);
+      toast.success("Registration Success, Please log in!")
+      navigate("/login")
+      } catch (error) {
+        console.log("Registration error: ", error);
+        if(error.response.status === 400){
+          toast.error(error.response.data)
+        }
       }
+    } else{
+      toast.error("Name contains letters only!!!")
     }
-  }
+      
+   }
+
+
 
   return (
     <>
